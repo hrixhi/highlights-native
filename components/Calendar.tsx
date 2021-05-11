d972578376497c0031be242d77006395d4f2760c
Hrishi <hrishivo@usc.edu>
2021-05-10 19:33:52 -0700

    title

--- components/Calendar.tsx
+++ components/Calendar.tsx
@@ -150,7 +150,7 @@ const CalendarX: React.FunctionComponent<{ [label: string]: any }> = (props: any
                 <Text
                     ellipsizeMode="tail"
                     style={{ color: '#a2a2aa', fontSize: 17, flex: 1, lineHeight: 25, paddingHorizontal: 20, }}>
-                    Events
+                    Planner
                                 </Text>
             </View>
             <ScrollView style={{
