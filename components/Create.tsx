9372cb763e48d572ed8aff2431f0bf568af7e394
Hrishi <hrishivo@usc.edu>
2021-07-03 02:42:03 -0700

    hrishi

--- components/Create.tsx
+++ components/Create.tsx
@@ -1092,7 +1092,7 @@ const Create: React.FunctionComponent<{ [label: string]: any }> = (props: any) =
                         }
                         <Text style={{
                             color: '#a2a2aa',
-                            fontSize: 12,
+                            fontSize: 11,
                             lineHeight: 30,
                             textAlign: 'right',
                             paddingRight: 10,
